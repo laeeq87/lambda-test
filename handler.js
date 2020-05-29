393fe38d786f04833dce04b2f7062ab8beee7571
laeequrrehamn <laeeq.rehman18@gmail.com>
2020-05-30 02:28:08 +0500

    update

--- handler.js
+++ handler.js
@@ -1,19 +1,13 @@
+'use strict'
 const moment = require("moment");
 
 module.exports.logger = async event => {
-  try {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-
-        message: "Lambda Test Function",
-        version: "v1.0",
-        timestamp: moment().unix()
-      })
-    };
-
-  } catch (error) {
-    console.log("Error in try catch", error);
-    return error;
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Lambda Test Function",
+      version: "v1.0",
+      timestamp: moment().unix()
+    })
+  };
 };
