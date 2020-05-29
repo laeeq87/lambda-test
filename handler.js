73b1de2cd07335b4f0ee23733f96a99fe3b773c8
laeequrrehamn <laeeq.rehman18@gmail.com>
2020-05-30 01:13:09 +0500

    modified handler

--- handler.js
+++ handler.js
@@ -1,18 +1,14 @@
 'use strict';
 
 module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  try {
+    return {
+      statusCode: 200,
+      body: `Lambda Test Function`
+    };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  } catch (error) {
+    console.log("Error in try catch", error);
+    return error;
+  }
 };
