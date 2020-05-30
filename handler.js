ca78a24158e3731f056ae3ad83183dbdf691540a
laeequrrehamn <laeeq.rehman18@gmail.com>
2020-05-30 17:07:46 +0500

    update the handler version

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ module.exports.logger = async event => {
     statusCode: 200,
     body: JSON.stringify({
       message: "Lambda Test Function",
-      version: "v2.0",
+      version: "v3.0",
       timestamp: moment().unix()
     })
   };
